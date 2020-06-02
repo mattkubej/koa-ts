ea3eb1d3c7c611862bbbebe8d1cdbbf1c931d182
Matt Kubej <mkubej@gmail.com>
2020-06-02 19:56:53 -0400

    introduce delegations

--- src/context.ts
+++ src/context.ts
@@ -4,10 +4,14 @@ import statuses from 'statuses';
 import Cookies from 'cookies';
 import http from 'http';
 import util from 'util';
+import { Socket } from "net";
+import accepts from 'accepts';
 
 import Application from './application';
 import Request from './request';
 import Response from './response';
+
+import contentDisposition from 'content-disposition';
 
 const COOKIES = Symbol('context#cookies');
 
@@ -21,18 +25,10 @@ export default class Context {
   public response: Response;
   public app: Application;
 
-  public status: number;
-  public length: number;
   public originalUrl: string;
-  public headerSent: boolean;
-  public writable: boolean;
-  public type: string;
   public state: object;
 
   public respond: boolean;
-  public body: any;
-  public method: string;
-  public message: string;
 
   constructor() {
     this.assert = httpAssert;
@@ -130,5 +126,256 @@ export default class Context {
 
   set cookies(_cookies) {
     this[COOKIES] = _cookies;
+  }
+
+  /* Response delegation */
+
+  public attachment(filename: string, options: contentDisposition.Options) {
+    this.response.attachment(filename, options);
+  }
+
+  public redirect(url: string, alt: string) {
+    this.response.redirect(url, alt);
+  }
+
+  public remove(field: string) {
+    this.response.remove(field);
+  }
+
+  public vary(field: string) {
+    this.response.vary(field);
+  }
+
+  public has(field: string): boolean {
+    return this.response.has(field);
+  }
+
+  public set(field: string | object | string[], val: string | number) {
+    this.response.set(field, val);
+  }
+
+  public append(field: string, val: string | string[]) {
+    this.response.append(field, val);
+  }
+
+  public flushHeaders() {
+    this.response.flushHeaders();
+  }
+
+  set status(code: number) {
+    this.response.status = code;
+  }
+
+  get status(): number {
+    return this.response.status;
+  }
+
+  set message(msg: string) {
+    this.response.message = msg;
+  }
+
+  get message(): string {
+    return this.response.message;
+  }
+
+  set body(val: any) {
+    this.response.body = val;
+  }
+
+  get body(): any {
+    return this.response.body;
+  }
+
+  set length(n: number) {
+    this.response.length = n;
+  }
+
+  get length(): number {
+    return this.response.length;
+  }
+
+  set type(type: string) {
+    this.response.type = type;
+  }
+
+  get type(): string {
+    return this.response.type;
+  }
+
+  set lastModified(val: Date) {
+    this.response.lastModified = val;
+  }
+
+  get lastModified(): Date | string {
+    return this.response.lastModified;
+  }
+
+  set etag(val: string) {
+    this.response.etag = val;
+  }
+
+  get etag(): string {
+    return this.response.etag;
+  }
+
+  get headerSent(): boolean {
+    return this.response.headerSent;
+  }
+
+  get writable(): boolean {
+    return this.response.writable;
+  }
+
+  /* Request delegation */
+
+  public acceptLanguages(...args: string[]): string | false {
+    return this.request.acceptsLanguages(...args);
+  }
+
+  public acceptsEncodings(...args: string[]): string | false {
+    return this.request.acceptsEncodings(...args);
+  }
+
+  public acceptsCharsets(...args: string[]): string | false {
+    return this.request.acceptsCharsets(...args);
+  }
+
+  public accepts(...args: string[]): string | false | string[] {
+    return this.request.accepts(...args);
+  }
+
+  public get(field: string): string | string[] {
+    return this.request.get(field);
+  }
+
+  public is(type: string, ...types: string[]): string | false | null {
+    return this.request.is(type, ...types);
+  }
+
+  // TODO: types
+  set querystring(str) {
+    this.request.querystring = str;
+  }
+
+  // TODO: types
+  get querystring() {
+    return this.request.querystring;
+  }
+
+  // TODO: why was this delegated access? no setter on request
+  get idempotent(): boolean {
+    return this.request.idempotent;
+  }
+
+  // TODO: why was this delegated access? no setter on request
+  get socket(): Socket {
+    return this.request.socket;
+  }
+
+  set search(str: string) {
+    this.request.search = str;
+  }
+
+  get search(): string {
+    return this.request.search;
+  }
+
+  set method(val: string) {
+    this.request.method = val;
+  }
+
+  get method(): string {
+    return this.request.method;
+  }
+
+  // TODO: types
+  set query(obj) {
+    this.request.query = obj;
+  }
+
+  // TODO: types
+  get query() {
+    return this.request.query;
+  }
+
+  set path(path: string) {
+    this.request.path = path;
+  }
+
+  get path() {
+    return this.request.path;
+  }
+
+  set url(url: string) {
+    this.request.url = url; 
+  }
+
+  get url(): string {
+    return this.request.url;
+  }
+
+  // TODO: types
+  set accept(obj) {
+    this.request.accept = obj;
+  }
+
+  get accept(): accepts.Accepts {
+    return this.request.accept;
+  }
+
+  get origin(): string {
+    return this.request.origin;
+  }
+
+  get href(): string {
+    return this.request.href;
+  }
+
+  get subdomains(): string[] {
+    return this.request.subdomains;
+  }
+
+  get protocol(): string {
+    return this.request.protocol;
+  }
+
+  get host(): string {
+    return this.request.host;
+  }
+
+  get hostname(): string {
+    return this.request.hostname;
+  }
+
+  get URL(): URL {
+    return this.request.URL;
+  }
+
+  get header(): http.IncomingHttpHeaders {
+    return this.request.header;
+  }
+
+  get headers(): http.IncomingHttpHeaders {
+    return this.request.headers;
+  }
+
+  get secure(): boolean {
+    return this.request.secure;
+  }
+
+  get stale(): boolean {
+    return this.request.stale;
+  }
+
+  get fresh(): boolean {
+    return this.request.fresh;
+  }
+
+  get ips(): string[] {
+    return this.request.ips;
+  }
+
+  get ip(): string {
+    return this.request.ip;
   }
 }
