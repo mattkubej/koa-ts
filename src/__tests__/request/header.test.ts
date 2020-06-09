49c821b9e211e5f5f08a023e9597c7ff013a829f
Matt Kubej <mkubej@gmail.com>
2020-06-09 00:58:50 -0400

    request header test

---
+++ src/__tests__/request/header.test.ts
@@ -0,0 +1,14 @@
+import { request } from '../helpers/context';
+
+describe('req.header', () => {
+  it('should return the request header object', () => {
+    const req = request();
+    expect(req.header).toStrictEqual(req.req.headers);
+  });
+
+  it('should set the request header object', () => {
+    const req = request();
+    req.header = {'X-Custom-Headerfield': 'Its one header, with headerfields'};
+    expect(req.header).toStrictEqual(req.req.headers);
+  });
+});
