afc9932090a28278f0cc616f0253e185464dac24
Matt Kubej <mkubej@gmail.com>
2020-06-09 00:57:23 -0400

    request get test

---
+++ src/__tests__/request/get.test.ts
@@ -0,0 +1,14 @@
+import context from '../helpers/context';
+
+describe('ctx.get(name)', () => {
+  it('should return the field value', () => {
+    const ctx = context();
+    ctx.req.headers.host = 'http://google.com';
+    ctx.req.headers.referer = 'http://google.com';
+    expect(ctx.get('HOST')).toBe('http://google.com');
+    expect(ctx.get('Host')).toBe('http://google.com');
+    expect(ctx.get('host')).toBe('http://google.com');
+    expect(ctx.get('referer')).toBe('http://google.com');
+    expect(ctx.get('referrer')).toBe('http://google.com');
+  });
+});
