48fe61fe37206225357e1848f68ccb304d3c2313
Matt Kubej <mkubej@gmail.com>
2020-06-09 23:20:47 -0400

    response set test

---
+++ src/__tests__/response/set.test.ts
@@ -0,0 +1,42 @@
+import context from '../helpers/context';
+
+describe('ctx.set(name, val)', () => {
+  it('should set a field value', () => {
+    const ctx = context();
+    ctx.set('x-foo', 'bar');
+    expect(ctx.response.header['x-foo']).toBe('bar');
+  });
+
+  it('should coerce number to string', () => {
+    const ctx = context();
+    ctx.set('x-foo', 5);
+    expect(ctx.response.header['x-foo']).toBe(5);
+  });
+
+  // TODO: is this necessary?
+  //it('should coerce undefined to string', () => {
+    //const ctx = context();
+    //ctx.set('x-foo', undefined);
+    //expect(ctx.response.header['x-foo']).toBe('undefined');
+  //});
+
+  it('should set a field value of array', () => {
+    const ctx = context();
+    ctx.set('x-foo', ['foo', 'bar']);
+    expect(ctx.response.header['x-foo']).toStrictEqual([ 'foo', 'bar' ]);
+  });
+});
+
+describe('ctx.set(object)', () => {
+  it('should set multiple fields', () => {
+    const ctx = context();
+
+    ctx.set({
+      foo: '1',
+      bar: '2'
+    });
+
+    expect(ctx.response.header.foo).toBe('1');
+    expect(ctx.response.header.bar).toBe('2');
+  });
+});
