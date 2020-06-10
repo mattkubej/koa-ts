3ce28b713009be2039d5a95ca2d2e1d78280f638
Matt Kubej <mkubej@gmail.com>
2020-06-09 23:07:22 -0400

    response message test

---
+++ src/__tests__/response/message.test.ts
@@ -0,0 +1,27 @@
+import { response } from '../helpers/context';
+
+describe('res.message', () => {
+  it('should return the response status message', () => {
+    const res = response();
+    res.status = 200;
+    expect(res.message).toBe('OK');
+  });
+
+  describe('when res.message not present', () => {
+    it('should look up in statuses', () => {
+      const res = response();
+      res.res.statusCode = 200;
+      expect(res.message).toBe('OK');
+    });
+  });
+});
+
+describe('res.message=', () => {
+  it('should set response status message', () => {
+    const res = response();
+    res.status = 200;
+    res.message = 'ok';
+    expect(res.res.statusMessage).toBe('ok');
+    expect(res.inspect().message).toBe('ok');
+  });
+});
