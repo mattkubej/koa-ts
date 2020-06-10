5a0bccb65be0708005d64946be3b9a750684cf12
Matt Kubej <mkubej@gmail.com>
2020-06-09 20:49:55 -0400

    request whatwg-url test

---
+++ src/__tests__/request/whatwg-url.test.ts
@@ -0,0 +1,23 @@
+import { request } from '../helpers/context';
+
+describe('req.URL', () => {
+  describe('should not throw when', () => {
+    it('host is void', () => {
+      // Accessing the URL should not throw.
+      request().URL;
+    });
+
+    it('header.host is invalid', () => {
+      const req = request();
+      req.header.host = 'invalid host';
+      // Accessing the URL should not throw.
+      req.URL;
+    });
+  });
+
+  it('should return empty object when invalid', () => {
+    const req = request();
+    req.header.host = 'invalid host';
+    expect(req.URL).toMatchObject(Object.create(null));
+  });
+});
