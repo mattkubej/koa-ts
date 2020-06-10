95a161551c04971028e97b598e402cad16acffbb
Matt Kubej <mkubej@gmail.com>
2020-06-09 20:07:49 -0400

    request href test

---
+++ src/__tests__/request/href.test.ts
@@ -0,0 +1,44 @@
+import http from 'http';
+import Koa from '../../application';
+import Context from '../../context';
+import context from '../helpers/context';
+
+describe('ctx.href', () => {
+  it('should return the full request url', () => {
+    const req = {
+      url: '/users/1?next=/dashboard',
+      headers: {
+        host: 'localhost'
+      },
+    };
+    const ctx = context(req);
+    expect(ctx.href).toBe('http://localhost/users/1?next=/dashboard');
+    // change it also work
+    ctx.url = '/foo/users/1?next=/dashboard';
+    expect(ctx.href).toBe('http://localhost/users/1?next=/dashboard');
+  });
+
+  it('should work with `GET http://example.com/foo`', done => {
+    const app = new Koa();
+    app.use((ctx: Context) => {
+      ctx.body = ctx.href;
+    });
+    app.listen(function(){
+      const address = this.address();
+      http.get({
+        host: 'localhost',
+        path: 'http://example.com/foo',
+        port: address.port
+      }, res => {
+        expect(res.statusCode).toBe(200);
+        let buf = '';
+        res.setEncoding('utf8');
+        res.on('data', s => buf += s);
+        res.on('end', () => {
+          expect(buf).toBe('http://example.com/foo');
+          done();
+        });
+      });
+    });
+  });
+});
