76f67f7252718147dc1e0e9df70666a65d2df590
Matt Kubej <mkubej@gmail.com>
2020-06-09 22:37:45 -0400

    response headers test

---
+++ src/__tests__/response/headers.test.ts
@@ -0,0 +1,17 @@
+import { response } from '../helpers/context';
+
+describe('res.header', () => {
+  it('should return the response header object', () => {
+    const res = response();
+    res.set('X-Foo', 'bar');
+    expect(res.headers).toMatchObject({ 'x-foo': 'bar' });
+  });
+
+  describe('when res._headers not present', () => {
+    it('should return empty object', () => {
+      const res = response();
+      res.res._headers = null;
+      expect(res.headers).toMatchObject({});
+    });
+  });
+});
