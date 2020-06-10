644199898966469d69f30d21d7f55dcc70191be9
Matt Kubej <mkubej@gmail.com>
2020-06-09 20:21:55 -0400

    request ips test

---
+++ src/__tests__/request/ips.test.ts
@@ -0,0 +1,67 @@
+import { request } from '../helpers/context';
+
+describe('req.ips', () => {
+  describe('when X-Forwarded-For is present', () => {
+    describe('and proxy is not trusted', () => {
+      it('should be ignored', () => {
+        const req = request();
+        req.app.proxy = false;
+        req.header['x-forwarded-for'] = '127.0.0.1,127.0.0.2';
+        expect(req.ips).toStrictEqual([]);
+      });
+    });
+
+    describe('and proxy is trusted', () => {
+      it('should be used', () => {
+        const req = request();
+        req.app.proxy = true;
+        req.header['x-forwarded-for'] = '127.0.0.1,127.0.0.2';
+        expect(req.ips).toStrictEqual(['127.0.0.1', '127.0.0.2']);
+      });
+    });
+  });
+
+  describe('when options.proxyIpHeader is present', () => {
+    describe('and proxy is not trusted', () => {
+      it('should be ignored', () => {
+        const req = request();
+        req.app.proxy = false;
+        req.app.proxyIpHeader = 'x-client-ip';
+        req.header['x-client-ip'] = '127.0.0.1,127.0.0.2';
+        expect(req.ips).toStrictEqual([]);
+      });
+    });
+
+    describe('and proxy is trusted', () => {
+      it('should be used', () => {
+        const req = request();
+        req.app.proxy = true;
+        req.app.proxyIpHeader = 'x-client-ip';
+        req.header['x-client-ip'] = '127.0.0.1,127.0.0.2';
+        expect(req.ips).toStrictEqual(['127.0.0.1', '127.0.0.2']);
+      });
+    });
+  });
+
+  describe('when options.maxIpsCount is present', () => {
+    describe('and proxy is not trusted', () => {
+      it('should be ignored', () => {
+        const req = request();
+        req.app.proxy = false;
+        req.app.maxIpsCount = 1;
+        req.header['x-forwarded-for'] = '127.0.0.1,127.0.0.2';
+        expect(req.ips).toStrictEqual([]);
+      });
+    });
+
+    describe('and proxy is trusted', () => {
+      it('should be used', () => {
+        const req = request();
+        req.app.proxy = true;
+        req.app.maxIpsCount = 1;
+        req.header['x-forwarded-for'] = '127.0.0.1,127.0.0.2';
+        expect(req.ips).toStrictEqual(['127.0.0.2']);
+      });
+    });
+  });
+});
