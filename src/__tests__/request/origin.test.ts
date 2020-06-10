9ec0a9d2e4f4e81b89c8d21d6bc7b0b26b74c75b
Matt Kubej <mkubej@gmail.com>
2020-06-09 20:30:51 -0400

    request origin test

---
+++ src/__tests__/request/origin.test.ts
@@ -0,0 +1,17 @@
+import context from '../helpers/context';
+
+describe('ctx.origin', () => {
+  it('should return the origin of url', () => {
+    const req = {
+      url: '/users/1?next=/dashboard',
+      headers: {
+        host: 'localhost'
+      },
+    };
+    const ctx = context(req);
+    expect(ctx.origin).toBe('http://localhost');
+    // change it also work
+    ctx.url = '/foo/users/1?next=/dashboard';
+    expect(ctx.origin).toBe('http://localhost');
+  });
+});
