f84d6b991116bf09c760a350d06d3cb84cfd409f
Matt Kubej <mkubej@gmail.com>
2020-06-09 20:32:42 -0400

    request path test

---
+++ src/__tests__/request/path.test.ts
@@ -0,0 +1,36 @@
+import context from '../helpers/context';
+import parseurl from 'parseurl';
+
+describe('ctx.path', () => {
+  it('should return the pathname', () => {
+    const ctx = context();
+    ctx.url = '/login?next=/dashboard';
+    expect(ctx.path).toBe('/login');
+  });
+});
+
+describe('ctx.path=', () => {
+  it('should set the pathname', () => {
+    const ctx = context();
+    ctx.url = '/login?next=/dashboard';
+
+    ctx.path = '/logout';
+    expect(ctx.path).toBe('/logout');
+    expect(ctx.url).toBe('/logout?next=/dashboard');
+  });
+
+  it('should change .url but not .originalUrl', () => {
+    const ctx = context({ url: '/login' });
+    ctx.path = '/logout';
+    expect(ctx.url).toBe('/logout');
+    expect(ctx.originalUrl).toBe('/login');
+    expect(ctx.request.originalUrl).toBe('/login');
+  });
+
+  it('should not affect parseurl', () => {
+    const ctx = context({ url: '/login?foo=bar' });
+    ctx.path = '/login';
+    const url = parseurl(ctx.req);
+    expect(url.path).toBe('/login?foo=bar');
+  });
+});
