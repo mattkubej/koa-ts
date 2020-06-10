41669cc6d219a770dbda388aec1128eb4a1f0202
Matt Kubej <mkubej@gmail.com>
2020-06-10 00:07:16 -0400

    response vary test

---
+++ src/__tests__/response/vary.test.ts
@@ -0,0 +1,31 @@
+import context from '../helpers/context';
+
+describe('ctx.vary(field)', () => {
+  describe('when Vary is not set', () => {
+    it('should set it', () => {
+      const ctx = context();
+      ctx.vary('Accept');
+      expect(ctx.response.header.vary).toBe('Accept');
+    });
+  });
+
+  describe('when Vary is set', () => {
+    it('should append', () => {
+      const ctx = context();
+      ctx.vary('Accept');
+      ctx.vary('Accept-Encoding');
+      expect(ctx.response.header.vary).toBe('Accept, Accept-Encoding');
+    });
+  });
+
+  describe('when Vary already contains the value', () => {
+    it('should not append', () => {
+      const ctx = context();
+      ctx.vary('Accept');
+      ctx.vary('Accept-Encoding');
+      ctx.vary('Accept');
+      ctx.vary('Accept-Encoding');
+      expect(ctx.response.header.vary).toBe('Accept, Accept-Encoding');
+    });
+  });
+});
