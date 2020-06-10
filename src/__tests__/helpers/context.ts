49fcd77f08fc1b66d938a812b4f37a50cea5cd35
Matt Kubej <mkubej@gmail.com>
2020-06-09 20:02:51 -0400

    request host test

--- src/__tests__/helpers/context.ts
+++ src/__tests__/helpers/context.ts
@@ -2,7 +2,7 @@ import { Socket } from 'net';
 import Koa from '../../application';
 import http from 'http';
 
-const context = (req?: http.IncomingMessage, res?: http.ServerResponse, app?: Koa) => {
+const context = (req?: any, res?: any, app?: Koa) => {
   const socket = new Socket();
   req = Object.assign(new http.IncomingMessage(socket), req);
   res = Object.assign(new http.ServerResponse(req), res);
@@ -11,5 +11,5 @@ const context = (req?: http.IncomingMessage, res?: http.ServerResponse, app?: Ko
 };
 
 export default context;
-export const request = (req?: http.IncomingMessage, res?: http.ServerResponse, app?: Koa) => context(req, res, app).request;
-export const response = (req?: http.IncomingMessage, res?: http.ServerResponse, app?: Koa) => context(req, res, app).response;
+export const request = (req?: any, res?: any, app?: Koa) => context(req, res, app).request;
+export const response = (req?: any, res?: any, app?: Koa) => context(req, res, app).response;
