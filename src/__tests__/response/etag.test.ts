aae09395176c755f53ebe85e3b8cec3d86b4067b
Matt Kubej <mkubej@gmail.com>
2020-06-09 22:28:04 -0400

    response etag test

---
+++ src/__tests__/response/etag.test.ts
@@ -0,0 +1,29 @@
+import { response } from '../helpers/context';
+
+describe('res.etag=', () => {
+  it('should not modify an etag with quotes', () => {
+    const res = response();
+    res.etag = '"asdf"';
+    expect(res.header.etag).toBe('"asdf"');
+  });
+
+  it('should not modify a weak etag', () => {
+    const res = response();
+    res.etag = 'W/"asdf"';
+    expect(res.header.etag).toBe('W/"asdf"');
+  });
+
+  it('should add quotes around an etag if necessary', () => {
+    const res = response();
+    res.etag = 'asdf';
+    expect(res.header.etag).toBe('"asdf"');
+  });
+});
+
+describe('res.etag', () => {
+  it('should return etag', () => {
+    const res = response();
+    res.etag = '"asdf"';
+    expect(res.etag).toBe('"asdf"');
+  });
+});
