9cbbd0584487c904b3c84bb9df0abb2ea993f37f
Matt Kubej <mkubej@gmail.com>
2020-06-09 20:39:28 -0400

    request query test

---
+++ src/__tests__/request/query.test.ts
@@ -0,0 +1,39 @@
+import context from '../helpers/context';
+
+describe('ctx.query', () => {
+  describe('when missing', () => {
+    it('should return an empty object', () => {
+      const ctx = context({ url: '/' });
+      expect(ctx.query).toMatchObject({});
+    });
+
+    it('should return the same object each time it\'s accessed', () => {
+      const ctx = context({ url: '/' });
+      ctx.query.a = '2';
+      expect(ctx.query.a).toBe('2');
+    });
+  });
+
+  it('should return a parsed query-string', () => {
+    const ctx = context({ url: '/?page=2' });
+    expect(ctx.query.page).toBe('2');
+  });
+});
+
+describe('ctx.query=', () => {
+  it('should stringify and replace the querystring and search', () => {
+    const ctx = context({ url: '/store/shoes' });
+    ctx.query = { page: 2, color: 'blue' };
+    expect(ctx.url).toBe('/store/shoes?page=2&color=blue');
+    expect(ctx.querystring).toBe('page=2&color=blue');
+    expect(ctx.search).toBe('?page=2&color=blue');
+  });
+
+  it('should change .url but not .originalUrl', () => {
+    const ctx = context({ url: '/store/shoes' });
+    ctx.query = { page: 2 };
+    expect(ctx.url).toBe('/store/shoes?page=2');
+    expect(ctx.originalUrl).toBe('/store/shoes');
+    expect(ctx.request.originalUrl).toBe('/store/shoes');
+  });
+});
