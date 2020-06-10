f691e34d8dba3f9ff6c038563ab7b7f394f98d6d
Matt Kubej <mkubej@gmail.com>
2020-06-09 22:53:32 -0400

    response last-modified test

---
+++ src/__tests__/response/last-modified.test.ts
@@ -0,0 +1,32 @@
+import { response } from '../helpers/context';
+
+describe('res.lastModified', () => {
+  it('should set the header as a UTCString', () => {
+    const res = response();
+    const date = new Date();
+    res.lastModified = date;
+    expect(res.header['last-modified']).toBe(date.toUTCString());
+  });
+
+  it('should work with date strings', () => {
+    const res = response();
+    const date = new Date();
+    res.lastModified = date.toString();
+    expect(res.header['last-modified']).toBe(date.toUTCString());
+  });
+
+  it('should get the header as a Date', () => {
+    // Note: Date() removes milliseconds, but it's practically important.
+    const res = response();
+    const date = new Date();
+    res.lastModified = date;
+    expect((res.lastModified.getTime() / 1000)).toBe(Math.floor(date.getTime() / 1000));
+  });
+
+  describe('when lastModified not set', () => {
+    it('should get undefined', () => {
+      const res = response();
+      expect(res.lastModified).toBeUndefined();
+    });
+  });
+});
