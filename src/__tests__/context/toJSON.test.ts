cee98b17d18d513faf2b7f581fea35510a951362
Matt Kubej <mkubej@gmail.com>
2020-06-10 22:33:47 -0400

    context toJSON test

---
+++ src/__tests__/context/toJSON.test.ts
@@ -0,0 +1,34 @@
+import context from '../helpers/context';
+
+describe('ctx.toJSON()', () => {
+  it('should return a json representation', () => {
+    const ctx = context();
+
+    ctx.req.method = 'POST';
+    ctx.req.url = '/items';
+    ctx.req.headers['content-type'] = 'text/plain';
+    ctx.status = 200;
+    ctx.body = '<p>Hey</p>';
+
+    const obj = JSON.parse(JSON.stringify(ctx));
+    const req = obj.request;
+    const res = obj.response;
+
+    expect({
+      method: 'POST',
+      url: '/items',
+      header: {
+        'content-type': 'text/plain'
+      }
+    }).toMatchObject(req);
+
+    expect({
+      status: 200,
+      message: 'OK',
+      header: {
+        'content-type': 'text/html; charset=utf-8',
+        'content-length': 10
+      }
+    }).toMatchObject(res);
+  });
+});
