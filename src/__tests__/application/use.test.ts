68e6748523bdba0a04cd07a04cea7693c6313527
Matt Kubej <mkubej@gmail.com>
2020-06-10 21:55:04 -0400

    application use test

--- src/__tests__/application/use.test.ts
+++ src/__tests__/application/use.test.ts
@@ -7,21 +7,21 @@ describe('app.use(fn)', () => {
     const app = new Koa();
     const calls = [];
 
-    app.use((ctx: Context, next: Function) => {
+    app.use((_: Context, next: Function) => {
       calls.push(1);
       return next().then(() => {
         calls.push(6);
       });
     });
 
-    app.use((ctx: Context, next: Function) => {
+    app.use((_: Context, next: Function) => {
       calls.push(2);
       return next().then(() => {
         calls.push(5);
       });
     });
 
-    app.use((ctx: Context, next: Function) => {
+    app.use((_: Context, next: Function) => {
       calls.push(3);
       return next().then(() => {
         calls.push(4);
@@ -42,7 +42,7 @@ describe('app.use(fn)', () => {
     const app = new Koa();
     const calls = [];
 
-    app.use((ctx: Context, next: Function) => {
+    app.use((_: Context, next: Function) => {
       calls.push(1);
       return next().then(() => {
         calls.push(6);
@@ -55,7 +55,7 @@ describe('app.use(fn)', () => {
       calls.push(5);
     });
 
-    app.use((ctx: Context, next: Function) => {
+    app.use((_: Context, next: Function) => {
       calls.push(3);
       return next().then(() => {
         calls.push(4);
@@ -86,8 +86,8 @@ describe('app.use(fn)', () => {
     process.once('deprecation', () => {}); // silence deprecation message
     const app = new Koa();
 
-    app.use((ctx: Context, next: Function) => next());
-    app.use(function * (next: Function){ this.body = 'generator'; });
+    app.use((_: Context, next: Function) => next());
+    app.use(function * (){ this.body = 'generator'; });
 
     return request(app.callback())
       .get('/')
@@ -95,21 +95,21 @@ describe('app.use(fn)', () => {
       .expect('generator');
   });
 
-  //it('should throw error for non function', () => {
-    //const app = new Koa();
+  it('should throw error for non function', () => {
+    const app = new Koa();
 
-    //[null, undefined, 0, false, 'not a function'].forEach(v => {
-      //assert.throws(() => app.use(v), /middleware must be a function!/);
-    //});
-  //});
+    [null, undefined, 0, false, 'not a function'].forEach(v => {
+      expect(() => (app as any).use(v)).toThrow(/middleware must be a function!/);
+    });
+  });
 
-  //it('should output deprecation message for generator functions', done => {
-    //process.once('deprecation', message => {
-      //assert(/Support for generators will be removed/.test(message));
-      //done();
-    //});
+  it('should output deprecation message for generator functions', done => {
+    process.once('deprecation', message => {
+      expect(/Support for generators will be removed/.test(String(message))).toBeTruthy();
+      done();
+    });
 
-    //const app = new Koa();
-    //app.use(function * (){});
-  //});
+    const app = new Koa();
+    app.use(function * (){});
+  });
 });
