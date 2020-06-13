07a1761088b18f92475cec8269fce3ce6e0d2435
Matt Kubej <mkubej@gmail.com>
2020-06-13 12:31:21 -0400

    fix eslint issue

--- src/__tests__/request/secure.test.ts
+++ src/__tests__/request/secure.test.ts
@@ -1,9 +1,11 @@
 import { request } from '../helpers/context';
+import { Socket } from 'net';
+import { TLSSocket } from 'tls';
 
 describe('req.secure', () => {
   it('should return true when encrypted', () => {
     const req = request();
-    req.req.socket = { encrypted: true };
+    req.req.socket = new TLSSocket(new Socket());
     expect(req.secure).toBeTruthy();
   });
 });
