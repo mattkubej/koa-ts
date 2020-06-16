ee9d0a1dd81394b3574b16262f8edf80fae1a3ae
Matt Kubej <mkubej@gmail.com>
2020-06-15 20:45:13 -0400

    context inspect test fix lint errors

--- src/__tests__/context/inspect.test.ts
+++ src/__tests__/context/inspect.test.ts
@@ -6,15 +6,5 @@ describe('ctx.inspect()', () => {
     const toJSON = ctx.toJSON();
 
     expect(toJSON).toMatchObject(ctx.inspect());
-
-    // Note: is this valid anymore?
-    // expect(util.inspect(toJSON)).toMatchObject(util.inspect(ctx));
   });
-
-  // Note: is this valid a test anymore?
-  // console.log(require.cache) will call prototype.inspect()
-  // it('should not crash when called on the prototype', () => {
-  // expect(Context).toMatchObject(Context.prototype.inspect());
-  // expect(util.inspect(Context.prototype.inspect())).toMatchObject(util.inspect(Context));
-  // });
 });
